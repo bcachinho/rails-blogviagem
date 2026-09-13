6129d71a41adc068f6ac259f414b361b3038cdc6
Bruno Cachinho <bruno.cachinho@gmail.com>
2026-09-13 02:09:12 -0300

    configura remetente dos emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # db/seeds.rb
 admins = [
-  { email: "bruno.cachinho@gmail.com", name: "Administrador", password: ENV.fetch("ADMIN1_PASSWORD") },
-  { email: "renato.viagista@gmail.com", name: "Administrador", password: ENV.fetch("ADMIN2_PASSWORD") }
+  { email: "bruno.cachinho@gmail.com", name: "Administrador", password: "senha_forte_do_primeiro_admin" },
+  { email: "renato.viagista@gmail.com", name: "Administrador", password: "senha_forte_do_segundo_admin" }
 ]
 
 admins.each do |data|
