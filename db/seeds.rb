6da4cf66a0aedb7ad3895bad162c613f7337e6c9
Bruno Cachinho <bruno.cachinho@gmail.com>
2026-09-13 01:46:07 -0300

    adicionar admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
 # db/seeds.rb
-User.create!(
-  name: "Admin",
-  email: "admin@blog.com",
-  password: "123456",
-  password_confirmation: "123456",
-  role: 1,
-  confirmed_at: Time.current # importante se estiver usando confirmable
-)
+admins = [
+  { email: "admin@blogdeviagem.com", name: "Administrador", password: ENV.fetch("ADMIN1_PASSWORD") },
+  { email: "outra-pessoa@example.com", name: "Segundo Admin", password: ENV.fetch("ADMIN2_PASSWORD") }
+]
+
+admins.each do |data|
+  admin = User.find_or_initialize_by(email: data[:email])
+  admin.name = data[:name]
+  admin.password = data[:password]
+  admin.password_confirmation = data[:password]
+  admin.role = :admin
+  admin.skip_confirmation! if admin.respond_to?(:skip_confirmation!)
+  admin.save!
+
+  puts "Usuário admin '#{data[:email]}' criado/atualizado com sucesso!"
+end
