54a0e3c9f2bf9dcfdf37fc9a7b885f1c0a2e7924
Bruno Cachinho <bruno.cachinho@gmail.com>
2026-09-13 01:24:43 -0300

    configura remetente dos emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "bruno.cachinho@gmail.com"
+  default from: ENV.fetch("MAILER_FROM", "bruno.cachinho@gmail.com")
   layout "mailer"
 end
