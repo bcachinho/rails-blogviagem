ae4faba2398baf803f67bfbb676e8303b0a9be03
Bruno Cachinho <bruno.cachinho@gmail.com>
2026-09-19 21:25:49 -0300

    Corrige busca de post por slug manual no FavoritesController

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,24 +9,24 @@ class FavoritesController < ApplicationController
     if @query.present?
       q = "%#{@query.downcase}%"
       favorites = favorites.joins("LEFT JOIN action_text_rich_texts
-                                    ON action_text_rich_texts.record_id = posts.id
-                                    AND action_text_rich_texts.record_type = 'Post'")
-                           .where("LOWER(posts.title) LIKE :q
-                                   OR LOWER(posts.excerpt) LIKE :q
-                                   OR LOWER(action_text_rich_texts.body) LIKE :q", q: q)
+                    ON action_text_rich_texts.record_id = posts.id
+                    AND action_text_rich_texts.record_type = 'Post'")
+                    .where("LOWER(posts.title) LIKE :q
+                    OR LOWER(posts.excerpt) LIKE :q
+                    OR LOWER(action_text_rich_texts.body) LIKE :q", q: q)
     end
 
     @favorites = favorites.order(published_at: :desc)
   end
 
   def create
-    post = Post.friendly.find(params[:post_id])
+    post = Post.find_by!(slug: params[:post_id])
     current_user.favorites.find_or_create_by(post: post)
     redirect_back fallback_location: posts_path, notice: "Post adicionado aos favoritos!"
   end
 
   def destroy
-    post = Post.friendly.find(params[:post_id])
+    post = Post.find_by!(slug: params[:post_id])
     favorite = current_user.favorites.find_by(post: post)
     favorite&.destroy
     redirect_back fallback_location: posts_path, notice: "Post removido dos favoritos!"
